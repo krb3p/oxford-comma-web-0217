f306737fca76c7e6689e103941a774119c340c6a
krb3p <ryankatherineburke@gmail.com>
2017-02-08 10:00:00 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  array_size = array.length
+  if array_size == 1
+    array [0]
+  elsif array_size == 2
+    array.join(" and ")
+  else
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  end
 end
